7e77c9d71e908d1ebfe2b6edeb198c9c483739c8
RandomUser <>
2021-06-29 23:23:41 +0800

    Added Model Modifications

--- app/subject/models.py
+++ app/subject/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date
+
 from django.db import models
 from django.db import models
 from django.conf import settings
@@ -6,8 +8,21 @@ from core.models import BaseModel
 
 class Subject(BaseModel):
     name = models.CharField(max_length=32)
-    duration = models.IntegerField(default=0)
-    start_date = models.TimeField(blank=True, null=True)
-    end_date = models.TimeField(blank=True, null=True)
+    # sections = models.ManytoManyField()
 
+
+class SubjectSchedule(BaseModel):
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    @property
+    def duration(self):
+        if not (self.end_time and self.start_time):
+            return 0
+        end_time = datetime.combine(date.min, self.end_time)
+        start_time = datetime.combine(date.min, self.start_time)
+        diff = end_time - start_time
+
+        return diff.seconds // 3600
 
