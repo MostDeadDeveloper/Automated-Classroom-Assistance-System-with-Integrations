4d5b5cfce4223294d2b3c60cf8b23ae9d1e0cbc0
RandomUser <>
2021-07-01 23:18:26 +0800

    Added Discord Auth API

--- app/api/urls/account.py
+++ app/api/urls/account.py
@@ -2,12 +2,21 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from django.urls import path
 
-from ..views.account import AccountListAPIView
+from ..views.account import (
+    AccountListAPIView,
+    ConvertDiscordIdToCredentialsAPIView,
+
+)
 
 account_patterns = format_suffix_patterns([
     path(
-        '/list',
+        'list',
         AccountListAPIView.as_view(),
         name='list',
+    ),
+    path(
+        'discord/auth',
+        ConvertDiscordIdToCredentialsAPIView.as_view(),
+        name='discord_id_to_creds',
     ),
 ])
--- app/api/views/account.py
+++ app/api/views/account.py
@@ -1,4 +1,8 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
 from ..serializers.account import AccountSerializer
 
@@ -8,3 +12,39 @@ from account.models import Account
 class AccountListAPIView(ListAPIView):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
+
+class ConvertDiscordIdToCredentialsAPIView(APIView):
+
+    def post(self, request):
+        discord_id = self.request.data.get('discord_id')
+        data = {}
+        data['discord_id'] = discord_id
+
+        existing_record = Account.objects.filter(discord_id=discord_id)
+
+        if existing_record:
+            account = existing_record.first()
+
+            existing_token = Token.objects.filter(user=account)
+
+            if existing_token:
+                data['token'] = existing_token.first().key
+
+
+            data['account_id'] = account.id
+            data['status'] = 'Success!'
+
+            return Response(
+                data=data,
+                status=status.HTTP_200_OK,
+            )
+        else:
+            data['status'] = 'Failed!'
+
+        return Response(
+            data=data,
+            status=status.HTTP_200_OK,
+        )
+
+
+
