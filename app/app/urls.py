72c11702659c5acf83bf8192cba15c422baddb7a
SalvoidAdmin <salvoid@admin.com>
2021-07-01 20:42:20 +0800

    Subject Views - Initial

--- app/app/urls.py
+++ app/app/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('api/', include('api.urls', namespace='api')),
     path('users/', include('django.contrib.auth.urls')),
     path('billboard/', include('billboard.urls')),
+    path('subject/', include('billboard.urls')),
     path('', BaseRedirectView.as_view(), name='index'),
 
 ]
