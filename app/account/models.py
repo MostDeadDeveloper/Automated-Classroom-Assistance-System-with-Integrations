28bf0766c3621c129636dc7b89958004aeab5a76
RandomUser <>
2021-07-02 15:37:39 +0800

    Revised Models for optimization

--- app/account/models.py
+++ app/account/models.py
@@ -9,11 +9,16 @@ from core.models import BaseModel
 
 
 class Section(BaseModel):
-    year_level = models.CharField(max_length=10)
+    year_level = models.CharField(max_length=10, null=True)
+    course_code = models.CharField(max_length=16, null=True)
+    couse_name = models.CharField(max_length=16, null=True)
+
+    def __str__(self):
+        return self.course_code
 
 
 class Student(BaseModel):
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True)
     account = models.OneToOneField(
         'account.Account',
         on_delete=models.CASCADE,
