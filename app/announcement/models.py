65c7ba7dcc1a4df98cac73f9c3fee13d1d185f69
RandomUser <>
2021-07-02 14:56:54 +0800

    New Announcement Model

---
+++ app/announcement/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime, date
+
+from django.db import models
+from django.conf import settings
+
+from core.models import BaseModel
+
+class Announcement(BaseModel):
+    name = models.CharField(max_length=32, blank=True, null=True)
+    related_section = models.ForeignKey(
+        'account.Section',
+        on_delete=models.CASCADE,
+    )
+    related_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    deadline_date = models.DateTimeField(blank=True, null=True)
+    is_repetitive = models.BooleanField(default=False)
+    content = models.CharField(max_length=100, blank=True)
+
+class SubjectSchedule(BaseModel):
+    CHOICES = (
+        ('Monday','Monday',),
+        ('Tuesday','Tuesday',),
+        ('Wednesday','Wednesday',),
+        ('Thursday','Thursday',),
+        ('Friday','Friday',),
+        ('Saturday','Saturday',),
+        ('Sunday','Sunday',),
+    )
+    day_of_the_week = models.CharField(max_length=16,choices=CHOICES,blank=True, null=True)
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
+
+
+    @property
+    def duration(self):
+        if not (self.end_time and self.start_time):
+            return 0
+        end_time = datetime.combine(date.min, self.end_time)
+        start_time = datetime.combine(date.min, self.start_time)
+        diff = end_time - start_time
+
+        return diff.seconds // 3600
+
