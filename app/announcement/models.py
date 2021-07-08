ce4a94fec21ab44371fdbbaaef319771a3a994b0
RandomUser <>
2021-07-09 01:03:30 +0800

    Added changes for model

--- app/announcement/models.py
+++ app/announcement/models.py
@@ -12,6 +12,7 @@ class Announcement(BaseModel):
         ('10Second','Second'),
     )
     frequency_type = models.CharField(max_length=20, choices=choices, blank=True, null=True)
+    repetition_count = models.IntegerField(default=0) # amount of times the announcement has already been repeated.
     max_frequency = models.IntegerField(default=0) # amount of times to be repeated, if is_repetitive is TRUE
     scheduled_date = models.DateTimeField(null=True)
     content = models.CharField(max_length=75, null=True)
